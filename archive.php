16d498141352ba0dcba42cdc3affac70e58e27a1
Derek Herman <derek@valendesigns.com>
2015-04-11 11:07:44 -0700

    Make use of WordPress 4.1 functions the_archive_title() and the_archive_description()

--- archive.php
+++ archive.php
@@ -18,66 +18,10 @@ get_header(); ?>
 
       <header class="page-header">
         <h1 class="page-title">
-          <?php
-          if ( is_category() ) :
-            single_cat_title();
-
-          elseif ( is_tag() ) :
-            single_tag_title();
-
-          elseif ( is_author() ) :
-            printf( __( 'Author: %s', 'archetype' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-          elseif ( is_day() ) :
-            printf( __( 'Day: %s', 'archetype' ), '<span>' . get_the_date() . '</span>' );
-
-          elseif ( is_month() ) :
-            printf( __( 'Month: %s', 'archetype' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'archetype' ) ) . '</span>' );
-
-          elseif ( is_year() ) :
-            printf( __( 'Year: %s', 'archetype' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'archetype' ) ) . '</span>' );
-
-          elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-            _e( 'Asides', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-            _e( 'Galleries', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-            _e( 'Images', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-            _e( 'Videos', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-            _e( 'Quotes', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-            _e( 'Links', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-            _e( 'Statuses', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-            _e( 'Audios', 'archetype' );
-
-          elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-            _e( 'Chats', 'archetype' );
-
-          else :
-            _e( 'Archives', 'archetype' );
-
-          endif;
-          ?>
+          <?php the_archive_title(); ?>
         </h1>
 
-        <?php
-        // Show an optional term description.
-        $term_description = term_description();
-        if ( ! empty( $term_description ) ) :
-          printf( '<div class="taxonomy-description">%s</div>', $term_description );
-        endif;
-        ?>
+        <?php the_archive_description( '<div class="archive-description">', '</div>' ); ?>
       </header><!-- .page-header -->
 
       <?php get_template_part( 'loop' ); ?>
