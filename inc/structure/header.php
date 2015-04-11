d1aee152e5c007b0201f43cd8050c6a10c8428e0
Derek Herman <derek@valendesigns.com>
2015-04-11 13:48:39 -0700

    escape!

--- inc/structure/header.php
+++ inc/structure/header.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'archetype_primary_navigation' ) ) {
   function archetype_primary_navigation() {
     ?>
     <nav id="site-navigation" class="main-navigation" role="navigation">
-      <button class="menu-toggle"><?php echo apply_filters( 'archetype_menu_toggle_text', __( 'Primary Menu', 'archetype' ) ); ?></button>
+      <button class="menu-toggle"><?php echo esc_attr( apply_filters( 'archetype_menu_toggle_text', __( 'Primary Menu', 'archetype' ) ) ); ?></button>
       <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
     </nav><!-- #site-navigation -->
     <?php
