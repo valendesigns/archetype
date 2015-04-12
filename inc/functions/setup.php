753f46ea7c7723b812d28d3b7c36ade83124a174
Derek Herman <derek@valendesigns.com>
2015-04-11 20:13:06 -0700

    html5 widgets

--- inc/functions/setup.php
+++ inc/functions/setup.php
@@ -79,11 +79,16 @@ if ( ! function_exists( 'archetype_setup' ) ) :
     ) );
     
     /*
-     * Switch default core markup for search form, comment form, and comments
-     * to output valid HTML5.
+     * Switch default core markup to output valid HTML5 for search form, comment form, 
+     * comments, galleries, captions and widgets.
      */
     add_theme_support( 'html5', array(
-      'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+      'search-form',
+      'comment-form',
+      'comment-list',
+      'gallery', 
+      'caption',
+      'widgets',
     ) );
 
     // Setup the WordPress core custom background feature.
