0c6ade6b097e115298b25d142ac6bb0092b11a58
Derek Herman <derek@valendesigns.com>
2015-04-14 19:37:12 -0700

    Woo Pagination. closes #50

--- inc/woocommerce/template-tags.php
+++ inc/woocommerce/template-tags.php
@@ -77,21 +77,21 @@ if ( ! function_exists( 'archetype_woocommerce_pagination' ) ) {
     echo '<nav class="woocommerce-pagination" role="navigation">';
       echo '<h2 class="screen-reader-text">Shop navigation</h2>';
       echo '<div class="nav-links">';
-    		echo paginate_links( apply_filters( 'woocommerce_pagination_args', array(
-    			'base'         => esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) ),
-    			'format'       => '',
-    			'add_args'     => '',
-    			'current'      => max( 1, get_query_var( 'paged' ) ),
-    			'total'        => $wp_query->max_num_pages,
-    			'prev_text'    => '&larr;',
-    			'next_text'    => '&rarr;',
-    			'type'         => 'plain',
-    			'end_size'     => 3,
-    			'mid_size'     => 3,
-    			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'archetype' ) . ' </span>',
+        echo paginate_links( apply_filters( 'woocommerce_pagination_args', array(
+          'base'         => esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) ),
+          'format'       => '',
+          'add_args'     => '',
+          'current'      => max( 1, get_query_var( 'paged' ) ),
+          'total'        => $wp_query->max_num_pages,
+          'prev_text'    => '&larr;',
+          'next_text'    => '&rarr;',
+          'type'         => 'plain',
+          'end_size'     => 3,
+          'mid_size'     => 3,
+          'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'archetype' ) . ' </span>',
           'after_page_number'  => ''
-    		) ) );
-		  echo '</div>';
+        ) ) );
+      echo '</div>';
     echo '</nav>';
   }
 }
