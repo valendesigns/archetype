394f73d0ef217a225348b7ef67db9e9a5bcff698
Derek Herman <derek@valendesigns.com>
2015-04-15 17:20:20 -0700

    moving tests

--- tests/phpunit/tests/theme.php
+++ tests/phpunit/tests/theme.php
@@ -6,14 +6,22 @@
  * @group theme
  */
 class Archetype_Tests_Theme extends WP_UnitTestCase {
-  
+
   /**
-   * Ensures the Archetype theme has been installed and activated by testing 
-   * if the `$archetype_version` global equals the current theme version.
+   * Check if 'Archetype' equals the current theme name.
+   */
+  function test_theme_name() {
+
+    $this->assertEquals( 'Archetype', wp_get_theme()->Name );
+
+  }
+
+  /**
+   * Check if the `$archetype_version` global equals the current theme version.
    */
   function test_theme_version() {
     global $archetype_version;
-    
+
     $this->assertEquals( $archetype_version, wp_get_theme()->Version );
 
   }
