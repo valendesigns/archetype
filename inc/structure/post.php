18570ae8fd9bd713089580069da59dd12d429557
Derek Herman <derek@valendesigns.com>
2015-04-17 20:04:50 -0700

    post thumbnail caption. closes #62

--- inc/structure/post.php
+++ inc/structure/post.php
@@ -23,21 +23,28 @@ if ( ! function_exists( 'archetype_post_format_media' ) ) {
     if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
       return;
     }
-  
+
+    $attachment = get_post( get_post_thumbnail_id() );
+    $caption = ! empty( $attachment->post_excerpt ) ? '<div class="caption"><span class="caption-body">' . wpautop( $attachment->post_excerpt ) . '</span></div>' : '';
+
     if ( is_singular() ) {
     ?>
   
     <div class="post-thumbnail">
-      <?php the_post_thumbnail(); ?>
+      <?php
+        the_post_thumbnail();
+        echo $caption;
+      ?>
     </div><!-- .post-thumbnail -->
   
     <?php } else { ?>
-  
-    <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
-      <?php
-        the_post_thumbnail( 'post-thumbnail', array( 'itemprop' => 'image', 'alt' => get_the_title() ) );
-      ?>
-    </a>
+
+    <div class="post-thumbnail">
+      <a href="<?php the_permalink(); ?>" aria-hidden="true">
+        <?php the_post_thumbnail( 'post-thumbnail', array( 'itemprop' => 'image', 'alt' => get_the_title() ) ); ?>
+      </a>
+      <?php echo $caption; ?>
+    </div>
   
     <?php } // End is_singular()
   }
