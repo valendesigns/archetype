fe16d8d6571f58a23a6889e25c06cfcf59d73e9d
Derek Herman <derek@valendesigns.com>
2015-04-23 03:16:52 -0700

    add hook for post meta

--- inc/structure/post.php
+++ inc/structure/post.php
@@ -128,7 +128,12 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
       if ( is_sticky() && is_home() && ! is_paged() ) {
         printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'archetype' ) );
       }
-    
+
+      /**
+       * Hook before the meta displays.
+       */
+      do_action( 'archetype_post_meta' );
+      
       $format = get_post_format();
       if ( current_theme_supports( 'post-formats', $format ) ) {
         printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
@@ -137,28 +142,28 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
           get_post_format_string( $format )
         );
       }
-    
+
       if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
         $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-    
+
         if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
           $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
         }
-    
+
         $time_string = sprintf( $time_string,
           esc_attr( get_the_date( 'c' ) ),
           get_the_date(),
           esc_attr( get_the_modified_date( 'c' ) ),
           get_the_modified_date()
         );
-    
+
         printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
           _x( 'Posted on', 'Used before publish date.', 'archetype' ),
           esc_url( get_permalink() ),
           $time_string
         );
       }
-    
+
       if ( 'post' == get_post_type() ) {
         if ( ( is_singular() || is_multi_author() ) && ! archetype_hide_author_bio() ) {
           printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',
@@ -167,7 +172,7 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
             get_the_author()
           );
         }
-    
+
         $categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'archetype' ) );
         if ( $categories_list && archetype_categorized_blog() ) {
           printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
@@ -175,7 +180,7 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
             $categories_list
           );
         }
-    
+
         $tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'archetype' ) );
         if ( $tags_list ) {
           printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
@@ -184,11 +189,11 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
           );
         }
       }
-    
+
       if ( is_attachment() && wp_attachment_is_image() ) {
         // Retrieve attachment metadata.
         $metadata = wp_get_attachment_metadata();
-    
+
         printf( '<span class="full-size-link"><span class="screen-reader-text">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
           _x( 'Full size', 'Used before full size attachment link.', 'archetype' ),
           esc_url( wp_get_attachment_url() ),
@@ -196,13 +201,13 @@ if ( ! function_exists( 'archetype_post_meta' ) ) {
           $metadata['height']
         );
       }
-    
+
       if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
         echo '<span class="comments-link">';
         comments_popup_link( __( 'Leave a comment', 'archetype' ), __( '1 Comment', 'archetype' ), __( '% Comments', 'archetype' ) );
         echo '</span>';
       }
-      
+
       edit_post_link( __( 'Edit', 'archetype' ), '<span class="edit-link">', '</span>' );
       ?>
     </aside>
