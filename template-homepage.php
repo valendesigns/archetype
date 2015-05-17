7ecc4775be3f07d9f1ffbe2d6caf5e07f3275ba0
Derek Herman <derek@valendesigns.com>
2015-05-17 15:28:29 -0700

    fix hook order

--- template-homepage.php
+++ template-homepage.php
@@ -19,12 +19,15 @@ get_header(); ?>
 
       <?php
       /**
+       * @hooked archetype_homepage_hero - 0
        * @hooked archetype_homepage_content - 10
-       * @hooked archetype_product_categories - 20
-       * @hooked archetype_recent_products - 30
-       * @hooked archetype_featured_products - 40
-       * @hooked archetype_popular_products - 50
-       * @hooked archetype_on_sale_products - 60
+       * @hooked archetype_homepage_custom_content - 20
+       * @hooked archetype_product_categories - 30
+       * @hooked archetype_recent_products - 40
+       * @hooked archetype_featured_products - 50
+       * @hooked archetype_popular_products - 60
+       * @hooked archetype_on_sale_products - 70
+       * @hooked archetype_homepage_custom_content_alt - 80
        */
       do_action( 'homepage' ); ?>
 
