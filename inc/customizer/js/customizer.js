89dc2509f3ff7d006c314280b1311384af97bfd8
Derek Herman <derek@valendesigns.com>
2015-05-17 19:17:40 -0700

    conditional boxed background. closes #87

--- inc/customizer/js/customizer.js
+++ inc/customizer/js/customizer.js
@@ -8,6 +8,10 @@
     init: function() {
       $( document ).on( 'click', 'input[name=customize-import-button]', Archetype_Customizer._import );
       $( document ).on( 'click', 'input[name=customize-export-button]', Archetype_Customizer._export );
+      $( document ).on( 'click', 'input[data-customize-setting-link=archetype_boxed]', Archetype_Customizer._toggle );
+      $( document ).ready( function() {
+        Archetype_Customizer._toggle();
+      });
     },
     _import: function() {
     var win     = $( window )
@@ -30,6 +34,15 @@
     _export: function() {
       window.location.href = Archetype_CustomizerConfig.customizerURL + '?customize-export=' + Archetype_CustomizerConfig.customizerExportNonce;
       return false;
+    },
+    _toggle: function() {
+      var input = $( 'input[data-customize-setting-link=archetype_boxed]' ),
+        color = $( '#customize-control-archetype_boxed_background_color' );
+      if ( input.prop( 'checked' ) ) {
+        color.show();
+      } else {
+        color.hide();
+      }
     }
   };
   $( Archetype_Customizer.init );
