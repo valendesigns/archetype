10454a4a857df0feca1112e04a77c33f65938697
Ryan Herman <ryan@valendesigns.com>
2015-05-28 16:03:02 -0700

    intro text

--- inc/admin/welcome-screen/sections/intro.php
+++ inc/admin/welcome-screen/sections/intro.php
@@ -7,14 +7,14 @@
  * @since 1.0.0
  */
 
-$storefront = wp_get_theme( 'archetype' );
+global $archetype_version;
 ?>
 <div class="col two-col" style="margin-bottom: 1.618em; overflow: hidden;">
 	<div class="col-1">
-		<h1 style="margin-right: 0;"><?php echo '<strong>Archetype</strong> <sup style="font-weight: bold; font-size: 50%; padding: 5px 10px; color: #666; background: #fff;">' . esc_attr( $storefront['Version'] ) . '</sup>'; ?></h1>
+		<h1 style="margin-right: 0;"><?php echo '<strong>Archetype</strong> <sup style="font-weight: bold; font-size: 50%; padding: 5px 10px; color: #666; background: #fff;">' . esc_attr( $archetype_version ) . '</sup>'; ?></h1>
 
-		<p style="font-size: 1.2em;"><?php _e( 'Awesome! You\'ve decided to use Storefront to enrich your WooCommerce store design.', 'archetype' ); ?></p>
-		<p><?php _e( 'Whether you\'re a store owner, WordPress developer, or both - we hope you enjoy Storefront\'s deep integration with WooCommerce core (including several popular WooCommerce extensions), plus the flexible design and extensible codebase that this theme provides.', 'archetype' ); ?>
+		<p style="font-size: 1.2em;"><?php _e( 'Welcome to Archetype! Where your websites functionality meets simplicity.', 'archetype' ); ?></p>
+		<p><?php _e( 'Itching to sell your products online? Our integrated WooCommerce core will assist you in doing just that. With support for many popular WooCommerce extensions and a beautiful design, you\'re just a few clicks away from your own branded shop. Also, our point and click Customizer options, backed by an extremely extensible codebase, will leave you with nothing but an ear to ear smile.', 'archetype' ); ?>
 	</div>
 
 	<div class="col-2 last-feature">
