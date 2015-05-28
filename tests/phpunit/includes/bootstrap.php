b1ae361b804eecc542748a91c26570dc4ccc06b2
Derek Herman <derek@valendesigns.com>
2015-05-28 01:16:34 -0700

    trying for best of both

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -7,17 +7,23 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+if ( empty( $_tests_dir ) ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
 if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
-	trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR );
+	// Testing with Core
+	$_tests_dir = '../../../../tests/phpunit';
+	if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
+		trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR );
+	}
 }
 
 require_once $_tests_dir . '/includes/functions.php';
 
-$basename = basename( dirname( __DIR__ ) );
-
 $GLOBALS['wp_tests_options'] = array(
-	'stylesheet' => $basename,
-	'template' => $basename
+	'stylesheet' => 'archetype',
+	'template' => 'archetype'
 );
 
 tests_add_filter( 'set_current_user', function( $arg ) {
