c8e4dcec182770005fc505f4a642412b6ae3c7d0
Derek Herman <derek@valendesigns.com>
2015-05-28 01:05:17 -0700

    trying something

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -5,11 +5,29 @@
  * @package Archetype
  */
 
-// Activates this theme in WordPress so it can be tested.
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
+	trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR );
+}
+
+require_once $_tests_dir . '/includes/functions.php';
+
+$basename = basename( dirname( __DIR__ ) );
+
 $GLOBALS['wp_tests_options'] = array(
-	'template'   => 'archetype',
-	'stylesheet' => 'archetype'
+	'stylesheet' => $basename,
+	'template' => $basename
 );
 
-// Load the WordPress test suite.
-require '../../../../tests/phpunit/includes/bootstrap.php';
+tests_add_filter( 'set_current_user', function( $arg ) {
+	$user = wp_get_current_user();
+	$user->set_role( 'administrator' );
+	return $arg;
+}, 1, 10 );
+
+tests_add_filter( 'filesystem_method', function( $arg ) {
+	return 'direct';
+}, 1, 10 );
+
+require $_tests_dir . '/includes/bootstrap.php';
