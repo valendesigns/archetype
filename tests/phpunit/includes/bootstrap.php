41bd1d57a52cada481920f112265dca2a40f96e5
Derek Herman <derek@valendesigns.com>
2015-05-28 01:28:08 -0700

    clean up

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -7,12 +7,13 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+// Travis testing.
 if ( empty( $_tests_dir ) ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Command line testing in Core.
 if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
-	// Testing with Core
 	$_tests_dir = '../../../../tests/phpunit';
 	if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
 		trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR );
@@ -21,19 +22,31 @@ if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
 
 require_once $_tests_dir . '/includes/functions.php';
 
+// Activate the theme.
 $GLOBALS['wp_tests_options'] = array(
 	'stylesheet' => 'archetype',
 	'template' => 'archetype'
 );
 
-tests_add_filter( 'set_current_user', function( $arg ) {
+/**
+ * Set the current user.
+ *
+ * @since 1.0.0
+ */
+function archetype_tests_set_current_user() {
 	$user = wp_get_current_user();
 	$user->set_role( 'administrator' );
-	return $arg;
-}, 1, 10 );
+}
+tests_add_filter( 'set_current_user', 'archetype_tests_set_current_user', 1, 10 );
 
-tests_add_filter( 'filesystem_method', function( $arg ) {
+/**
+ * Sets the filesystem method.
+ *
+ * @since 1.0.0
+ */
+function archetype_tests_filesystem_method() {
 	return 'direct';
-}, 1, 10 );
+}
+tests_add_filter( 'filesystem_method', 'archetype_tests_filesystem_method', 1, 10 );
 
 require $_tests_dir . '/includes/bootstrap.php';
