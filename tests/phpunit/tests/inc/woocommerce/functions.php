9afde9a213e2a3d6bb87d0f00e0a645285652e13
Derek Herman <derek@valendesigns.com>
2015-05-31 19:40:40 -0700

    remaining tests. closes #36

---
+++ tests/phpunit/tests/inc/woocommerce/functions.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * Test WooCommerce functions.
+ *
+ * @package Archetype
+ * @group woocommerce
+ */
+
+class Tests_WooCommerce_Functions extends WP_UnitTestCase {
+
+	/**
+	 * Check if viewing a WooCommerce page.
+	 */
+	function test_archetype_is_woocommerce() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Adds breadcrumbs depending on the value set in the customizer.
+	 */
+	function test_archetype_breadcrumb() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Product review arguments passed to the comments list.
+	 */
+	function test_archetype_product_review_list_args() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Before Content
+	 */
+	function test_archetype_before_content() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * After Content
+	 */
+	function test_archetype_after_content() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Before product loop
+	 */
+	function test_archetype_before_product_loop() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * After product loop
+	 */
+	function test_archetype_after_product_loop() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Before product loop item
+	 */
+	function test_archetype_before_product_loop_item() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * After product loop item
+	 */
+	function test_archetype_after_product_loop_item() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Before product loop item buttons
+	 */
+	function test_archetype_before_product_loop_item_buttons() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * After product loop item buttons
+	 */
+	function test_archetype_after_product_loop_item_buttons() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Default loop columns on product archives
+	 */
+	function test_archetype_loop_columns() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Adds various classes to the body tag
+	 */
+	function test_archetype_woocommerce_body_class() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Adds a column class to the post tag
+	 */
+	function test_archetype_woocommerce_post_class() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Replaces the placeholder image.
+	 */
+	function test_archetype_fix_thumbnail() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Returns the category placeholder image path.
+	 */
+	function test_archetype_woocommerce_placeholder_img_src() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Replaces `get_search_form()` with Product Search.
+	 */
+	function test_archetype_product_search_form() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Cart Fragments
+	 */
+	function test_archetype_cart_link_fragment() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * WooCommerce specific scripts & stylesheets
+	 */
+	function test_archetype_woocommerce_scripts() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Related Products Args
+	 */
+	function test_archetype_related_products_args() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Breadcrumb Defaults
+	 */
+	function test_archetype_breadcrumbs_defaults() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Product gallery thumnail columns
+	 */
+	function test_archetype_thumbnail_columns() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Products per page
+	 */
+	function test_archetype_products_per_page() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Check whether a WooCommerce extension is activated or not
+	 */
+	function test_is_woocommerce_extension_activated() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+}
---
+++ tests/phpunit/tests/inc/woocommerce/template-tags.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Test WooCommerce Template Tag functions.
+ *
+ * @package Archetype
+ * @group woocommerce
+ */
+
+class Tests_WooCommerce_Template_Tags extends WP_UnitTestCase {
+
+	/**
+	 * Display Product Categories
+	 */
+	function test_archetype_product_categories() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display Recent Products
+	 */
+	function test_archetype_recent_products() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display Featured Products
+	 */
+	function test_archetype_featured_products() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display Top Rated Products
+	 */
+	function test_archetype_top_rated_products() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display On Sale Products
+	 */
+	function test_archetype_on_sale_products() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Cart Link
+	 */
+	function test_archetype_cart_link() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display Header Cart
+	 */
+	function test_archetype_header_cart() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Upsells
+	 */
+	function test_archetype_upsell_display() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Display navigation to next/previous set of posts when applicable.
+	 */
+	function test_archetype_woocommerce_pagination() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Shop heading markup
+	 */
+	function test_archetype_shop_heading() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Shop heading wrapper
+	 */
+	function test_archetype_shop_heading_wrapper() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Shop heading wrapper close
+	 */
+	function test_archetype_shop_heading_wrapper_close() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Sorting wrapper
+	 */
+	function test_archetype_sorting_wrapper() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Sorting wrapper close
+	 */
+	function test_archetype_sorting_wrapper_close() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+	/**
+	 * Storefront shop messages
+	 */
+	function test_archetype_shop_messages() {
+
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+
+	}
+
+}
