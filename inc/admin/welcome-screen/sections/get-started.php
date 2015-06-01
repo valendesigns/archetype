9116127b5f5d80be42c47d266cebf510bf81edff
Ryan Herman <ryan@valendesigns.com>
2015-05-31 20:20:31 -0700

    get started page

--- inc/admin/welcome-screen/sections/get-started.php
+++ inc/admin/welcome-screen/sections/get-started.php
@@ -16,47 +16,47 @@ $url 	.= '&archetype-customizer=true';
 <div id="get_started" class="col two-col panel" style="margin-bottom: 1.618em; padding-top: 1.618em; overflow: hidden;">
 
 	<h2><?php _e( 'Using Archetype', 'archetype' ); ?> <div class="dashicons dashicons-heart" style="margin-top: .25em;"></div></h2>
-	<p><?php _e( 'We\'ve purposely kept Archetype lean & mean so configuration is a breeze. Here are some common theme-setup tasks:', 'archetype' ); ?></p>
+	<p><?php _e( 'To keep the simplicity alive, here\'s some common theme tasks.', 'archetype' ); ?></p>
 
 	<div class="col-1">
 		<?php if ( ! class_exists( 'WooCommerce' ) ) { ?>
-			<h4><?php _e( 'Install WooCommerce' ,'archetype' ); ?></h4>
-			<p><?php _e( 'Although Archetype works fine as a standard WordPress theme, it really shines when used for an online store. Install WooCommerce and start selling now.', 'archetype' ); ?></p>
+			<h4><?php _e( 'Include WooCommerce' ,'archetype' ); ?></h4>
+			<p><?php _e( 'Looking to include a online store for your project? Then WooCommerce is your first stop.', 'archetype' ); ?></p>
 
-			<p><a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=woocommerce' ), 'install-plugin_woocommerce' ) ); ?>" class="button button-primary"><?php _e( 'Install WooCommerce', 'archetype' ); ?></a></p>
+			<p><a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=woocommerce' ), 'install-plugin_woocommerce' ) ); ?>" class="button button-primary"><?php _e( 'Install', 'archetype' ); ?></a></p>
 		<?php } ?>
 
-		<h4><?php _e( 'Configure menu locations' ,'archetype' ); ?></h4>
-		<p><?php _e( 'Archetype includes three menu locations for primary, secondary and handheld navigation. The primary navigation is perfect for your key pages like the shop and product categories. The secondary navigation is better suited to lower traffic pages such as terms and conditions. The handheld navigation gives you complete control over what menu items to serve to handheld devices.', 'archetype' ); ?></p>
-		<p><a href="<?php echo esc_url( self_admin_url( 'nav-menus.php' ) ); ?>" class="button"><?php _e( 'Configure menus', 'archetype' ); ?></a></p>
+		<h4><?php _e( 'Menu configuration' ,'archetype' ); ?></h4>
+		<p><?php _e( 'Archetype includes three menu options used for navigating your web site. The Primary menu is used for your more commonly visited pages. The Secondary menu is used for pages you expect to see less traffic. The Handheld menu is used to select the pages you wish users to see when visiting your site on a handheld device.', 'archetype' ); ?></p>
+		<p><a href="<?php echo esc_url( self_admin_url( 'nav-menus.php' ) ); ?>" class="button"><?php _e( 'Configure', 'archetype' ); ?></a></p>
 
-		<h4><?php _e( 'Create a color scheme' ,'archetype' ); ?></h4>
-		<p><?php _e( 'Using the WordPress Customizer you can tweak Archetype\'s appearance to match your brand.', 'archetype' ); ?></p>
-		<p><a href="<?php echo esc_url( $url ); ?>" class="button"><?php _e( 'Open the Customizer', 'archetype' ); ?></a></p>
+		<h4><?php _e( 'Customizer' ,'archetype' ); ?></h4>
+		<p><?php _e( 'Not a CSS or HTML guru? Not a Problem.  Archetype\'s customizer provides effortless tact to match the color scheme and layout to your brand. Point, click, and admire.', 'archetype' ); ?></p>
+		<p><a href="<?php echo esc_url( $url ); ?>" class="button"><?php _e( 'Customize', 'archetype' ); ?></a></p>
 
 		<?php if ( ! class_exists( 'Jetpack' ) || ! class_exists( 'Archetype_Site_Logo' ) ) { ?>
-			<h4><?php _e( 'Add your logo', 'archetype' ); ?></h4>
-			<p><?php echo sprintf( esc_html__( 'Install and activate either %sJetpack%s or the to enable a custom logo option in the Customizer.', 'archetype' ), '<a href="https://wordpress.org/plugins/jetpack/">', '</a>' ); ?></p>
+			<h4><?php _e( 'Jetpack', 'archetype' ); ?></h4>
+			<p><?php echo sprintf( esc_html__( 'Looking to add a custom logo? Install and activate this great plugin by Wordpress.com. You may also enable a custom logo inside the Customizer.', 'archetype' ), '<a href="https://wordpress.org/plugins/jetpack/">', '</a>' ); ?></p>
 			<p>
 				<?php if ( ! class_exists( 'Jetpack' ) ) { ?>
-					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=jetpack' ), 'install-plugin_jetpack' ) ); ?>" class="button button-primary"><?php _e( 'Install Jetpack', 'archetype' ); ?></a>
+					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=jetpack' ), 'install-plugin_jetpack' ) ); ?>" class="button button-primary"><?php _e( 'Install', 'archetype' ); ?></a>
 				<?php } ?>
 			</p>
 		<?php } ?>
 	</div>
 
 	<div class="col-2 last-feature">
-		<h4><?php _e( 'Configure homepage template', 'archetype' ); ?></h4>
-		<p><?php _e( 'Archetype includes a homepage template that displays a selection of products from your store.', 'archetype' ); ?></p>
-		<p><?php echo sprintf( esc_html__( 'To set this up you will need to create a new page and assign the "Homepage" template to it. You can then set that as a static homepage in the %sReading%s settings.', 'archetype' ), '<a href="' . esc_url( self_admin_url( 'options-reading.php' ) ) . '">', '</a>' ); ?></p>
-		<p><?php echo sprintf( esc_html__( 'Once set up you can toggle and re-order the homepage components using the %sHomepage Control%s plugin.', 'archetype' ), '<a href="https://wordpress.org/plugins/homepage-control/">', '</a>' ); ?></p>
+		<h4><?php _e( 'Homepage control', 'archetype' ); ?></h4>
+		<p><?php _e( 'This is a fantastic tool for ordering content on the homepage of your site.', 'archetype' ); ?></p>
+		<p><?php echo sprintf( esc_html__( 'Assign the "Homepage" template to a new or existing page. Then set that as a static homepage in the Reading settings.', 'archetype' ), '<a href="' . esc_url( self_admin_url( 'options-reading.php' ) ) . '">', '</a>' ); ?></p>
+		<p><?php echo sprintf( esc_html__( 'Once Homepage Control is installed and activated, you can arrange content components in the order you see fit. This can be done from within the customizer.', 'archetype' ), '<a href="https://wordpress.org/plugins/homepage-control/">', '</a>' ); ?></p>
 
 		<?php if ( ! class_exists( 'Homepage_Control' ) ) { ?>
-			<p><a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=homepage-control' ), 'install-plugin_homepage-control' ) ); ?>" class="button button-primary"><?php _e( 'Install Homepage Control', 'archetype' ); ?></a></p>
+			<p><a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=homepage-control' ), 'install-plugin_homepage-control' ) ); ?>" class="button button-primary"><?php _e( 'Install', 'archetype' ); ?></a></p>
 		<?php } ?>
 
-		<h4><?php _e( 'View documentation', 'archetype' ); ?></h4>
-		<p><?php _e( 'You can read detailed information on Archetypes features and how to develop on top of it in the documentation.', 'archetype' ); ?></p>
-		<p><a href="http://docs.woothemes.com/documentation/themes/archetype/" class="button"><?php _e( 'View documentation &rarr;', 'archetype' ); ?></a></p>
+		<h4><?php _e( 'Documentation', 'archetype' ); ?></h4>
+		<p><?php _e( 'Interested in Archetype\'s features?', 'archetype' ); ?></p>
+		<p><a href="http://docs.woothemes.com/documentation/themes/archetype/" class="button"><?php _e( 'View Documentation', 'archetype' ); ?></a></p>
 	</div>
 </div>
