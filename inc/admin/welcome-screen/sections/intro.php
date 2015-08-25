5cc2327f58c2c63b5886199222f331febdde76d0
Derek Herman <derek@valendesigns.com>
2015-08-24 19:46:58 -0700

    styles

--- inc/admin/welcome-screen/sections/intro.php
+++ inc/admin/welcome-screen/sections/intro.php
@@ -11,7 +11,7 @@ global $archetype_version;
 ?>
 <div class="two-col" style="margin-bottom: 1.618em; overflow: hidden;">
 	<div class="col">
-		<h1 style="margin-right: 0;"><?php echo '<strong>Archetype</strong> <sup style="font-weight: bold; font-size: 50%; padding: 5px 10px; color: #666; background: #fff;">' . esc_attr( $archetype_version ) . '</sup>'; ?></h1>
+		<h1 style="margin-right: 0;"><?php echo '<strong>Archetype</strong> <sup style="font-weight: bold; font-size: 33.333%; padding: 5px 10px; color: #666; background: #fff; white-space: nowrap;">' . esc_attr( $archetype_version ) . '</sup>'; ?></h1>
 
 		<p style="font-size: 1.2em;"><?php esc_html_e( 'Welcome to Archetype! You\'ve made a great choice.', 'archetype' ); ?></p>
 		<p><?php esc_html_e( 'Archetype is more than just a beautiful blog! If you\'re wanting to setup shop, then our integrated WooCommerce core will assist you in doing just that. With support for many popular WooCommerce extensions and a beautiful design, you\'re just a few clicks away from your own branded online store.', 'archetype' ); ?></p>
