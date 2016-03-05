2c49938131d6f2c312098b57460c614ab91c1f43
Derek Herman <derek@valendesigns.com>
2016-03-05 14:04:18 -0800

    Hide pagination in shop when empty

--- inc/woocommerce/template-tags.php
+++ inc/woocommerce/template-tags.php
@@ -348,27 +348,32 @@ if ( ! function_exists( 'archetype_woocommerce_pagination' ) ) :
 		if ( ! woocommerce_products_will_display() ) {
 			return;
 		}
+
+		$links = paginate_links( apply_filters( 'woocommerce_pagination_args', array(
+			'base'               => esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) ),
+			'format'             => '',
+			'add_args'           => '',
+			'current'            => max( 1, get_query_var( 'paged' ) ),
+			'total'              => $wp_query->max_num_pages,
+			'prev_text'          => '&larr;',
+			'next_text'          => '&rarr;',
+			'type'               => 'plain',
+			'end_size'           => 3,
+			'mid_size'           => 3,
+			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'archetype' ) . ' </span>',
+			'after_page_number'  => '',
+		) ) );
+
+		if ( $links ) {
 		?>
-		<nav class="woocommerce-pagination" role="navigation">
-			<h2 class="screen-reader-text">Shop navigation</h2>
-			<div class="nav-links">
-			<?php echo paginate_links( apply_filters( 'woocommerce_pagination_args', array(
-				'base'               => esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) ),
-				'format'             => '',
-				'add_args'           => '',
-				'current'            => max( 1, get_query_var( 'paged' ) ),
-				'total'              => $wp_query->max_num_pages,
-				'prev_text'          => '&larr;',
-				'next_text'          => '&rarr;',
-				'type'               => 'plain',
-				'end_size'           => 3,
-				'mid_size'           => 3,
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'archetype' ) . ' </span>',
-				'after_page_number'  => '',
-			) ) ); ?>
-			</div>
-		</nav>
+			<nav class="woocommerce-pagination" role="navigation">
+				<h2 class="screen-reader-text">Shop navigation</h2>
+				<div class="nav-links">
+				<?php echo $links; ?>
+				</div>
+			</nav>
 		<?php
+		}
 	}
 endif;
 
